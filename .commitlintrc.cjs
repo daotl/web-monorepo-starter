5c801116596fb8debb1a273cb1c6e6493b4731e9
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-11-26 00:46:22 +0800

    setup(root): add "commitizen" with "commitiquette"

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,33 +1,37 @@
+// eslint-disable-next-line import/no-unresolved
 import { getPackages } from 'commitlint-config-pnpm-workspace/scope-enhanced'
 
+const pkgs = (ctx) => getPackages(ctx).then((pkgs) => pkgs.concat(['root']))
+
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    plugins: ['scope-enhanced'],
-    ignores: [(message) => message.startsWith('WIP: ')],
-    rules: {
-        'type-case': [0],
-        'type-enum': [
-            2,
-            'always',
-            [
-                'WIP',
-                'setup',
-                'build',
-                'docker',
-                'k8s',
-                'chore',
-                'ci',
-                'docs',
-                'feat',
-                'fix',
-                'perf',
-                'refactor',
-                'revert',
-                'style',
-                'test',
-            ],
-        ],
-        'scope-empty': [2, 'never'],
-        'scope-enum-enhanced': async (ctx) => [2, 'always', [...(await getPackages(ctx)), 'root']],
-    },
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['scope-enhanced'],
+  ignores: [(message) => message.startsWith('WIP: ')],
+  rules: {
+    'type-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'WIP',
+        'setup',
+        'build',
+        'docker',
+        'k8s',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-enum': async (ctx) => [0, 'always', await pkgs(ctx)],
+    'scope-enum-enhanced': async (ctx) => [2, 'always', await pkgs(ctx)],
+  },
 }
