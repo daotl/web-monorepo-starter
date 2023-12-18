63eab7dbb2a864ed2aa9a743c8c1a93fa9aa9588
Nex Zhu <hitnexup@gmail.com>
2023-12-18 10:11:16 +0800

    setup(@daotl/myutil): use @nx/js:tsc to build and @nx/vite:test to test

---
+++ lib/myutil/src/lib/myutil.spec.ts
@@ -0,0 +1,7 @@
+import { myutil } from './myutil'
+
+describe('myutil', () => {
+  it('should work', () => {
+    expect(myutil()).toEqual('myutil')
+  })
+})
---
+++ lib/myutil/src/lib/myutil.ts
@@ -0,0 +1,3 @@
+export function myutil(): string {
+  return 'myutil'
+}
